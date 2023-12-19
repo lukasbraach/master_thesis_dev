1e0ea300d02d00482e0f84a96d912a966f3cb625
Lukas Braach <lbraach@herewith.com>
2023-12-19 15:13:05 +0100

    upd: model seems to generate stuff!

--- src/models/components/sign_language_net.py
+++ src/models/components/sign_language_net.py
@@ -1,7 +1,7 @@
 from transformers import SpeechEncoderDecoderModel
 
 from src.models.components.language_decoder import LanguageDecoder
-from src.models.components.spatiotemporal_encoder import SpatiotemporalEncoder
+from src.models.components.spatiotemporal_encoder import SpatiotemporalEncoder, SpatiotemporalEncoderConfig
 
 
 class SignLanguageNet(SpeechEncoderDecoderModel):
@@ -9,7 +9,7 @@ class SignLanguageNet(SpeechEncoderDecoderModel):
     def __init__(
             self,
     ) -> None:
-        encoder = SpatiotemporalEncoder()
+        encoder = SpatiotemporalEncoder(SpatiotemporalEncoderConfig())
         decoder = LanguageDecoder()
 
         super().__init__(encoder=encoder, decoder=decoder)
