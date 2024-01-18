8554e5d62d28f2735a0bff9f9daa878ba370f918
Lukas Braach <lukas.braach@studium.uni-hamburg.de>
2024-01-18 15:11:37 +0100

    upd: sharding seems to somewhat work

---
+++ src/utils/class_set.py
@@ -0,0 +1,17 @@
+import functools
+from typing import List, Set
+
+import hydra
+
+
+def new(classpaths: List[str]) -> Set[type]:
+    result = []
+
+    for path in classpaths:
+        cls = hydra.utils.get_class(path)
+        result.append(cls)
+
+    return set(result)
+
+def part():
+    functools.partial
