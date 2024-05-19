92ea3fb97afd3771b8c5e22352890fb859e6907d
Lukas Braach <lukas.braach@netlight.com>
2024-05-19 18:21:58 +0200

    Try: Graceful frame error handling

--- data/bundestag_slr/process_videos.py
+++ data/bundestag_slr/process_videos.py
@@ -2,8 +2,8 @@ import csv
 import logging
 import os
 from collections import deque
+from concurrent.futures import ThreadPoolExecutor, Future
 from typing import List
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import av
 import cv2
@@ -20,14 +20,17 @@ pre_start_seconds = 0
 post_end_seconds = 1.5
 
 # Max thread workers
-max_workers = 16
+max_workers = 12
+
 
 # Function to initialize a MediaPipe FaceDetection instance
 def create_face_detection():
     mp_face_detection = mp.solutions.face_detection
     return mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5)
 
+
 face_detectors = [create_face_detection() for _ in range(max_workers)]
+
 
 def get_subtitle_items(subs: SubRipFile) -> List[SubRipItem]:
     items: List[SubRipItem] = []
@@ -38,26 +41,31 @@ def get_subtitle_items(subs: SubRipFile) -> List[SubRipItem]:
 
     return items
 
+
 def get_sample_aspect_ratio(video_path: str) -> float:
     container = av.open(video_path)
     video_stream = next(s for s in container.streams if s.type == 'video')
     return video_stream.sample_aspect_ratio.numerator / video_stream.sample_aspect_ratio.denominator
+
 
 def crop_signer(frame):
     height, width, _ = frame.shape
     right_half = frame[:, int(3 * width / 5):]
     return right_half
 
+
 def moving_average(bounding_box_buffer):
     avg_bbox = np.mean(bounding_box_buffer, axis=0)
     return avg_bbox
 
-def process_segment(video_path: str, subtitle_item: SubRipItem, frame_rate: float, frame_width: int, frame_height: int, frame_size_multiplier: float, face_detection, output_folder: str, idx: int):
+
+def process_segment(video_path: str, subtitle_item: SubRipItem, frame_rate: float, frame_width: int, frame_height: int,
+                    frame_size_multiplier: float, face_detection, output_folder: str, idx: int):
     start_time = subtitle_item.start.ordinal / 1000  # convert milliseconds to seconds
     end_time = subtitle_item.end.ordinal / 1000  # convert milliseconds to seconds
 
-    start_time = start_time - pre_start_seconds
-    end_time = end_time + post_end_seconds
+    start_time -= pre_start_seconds
+    end_time += post_end_seconds
 
     start_frame = int(start_time * frame_rate)
     end_frame = int(end_time * frame_rate)
@@ -87,23 +95,33 @@ def process_segment(video_path: str, subtitle_item: SubRipItem, frame_rate: floa
 
         if i % frame_rate_divisor == 0:
             signer_frame = crop_signer(frame)
-            width_diff_rel = (frame.shape[1] - signer_frame.shape[1]) / frame.shape[1]
 
-            results = face_detection.process(cv2.cvtColor(signer_frame, cv2.COLOR_BGR2RGB))
+            if signer_frame is None or signer_frame.size == 0:
+                logging.error(f"Signer frame is empty at frame {i}. Skipping.")
+                continue
 
-            if results.detections:
-                detection = results.detections[0]
-                bbox = detection.location_data.relative_bounding_box
+            try:
+                signer_frame_rgb = cv2.cvtColor(signer_frame, cv2.COLOR_BGR2RGB)
+                results = face_detection.process(signer_frame_rgb)
 
-                bbox.xmin = width_diff_rel + bbox.xmin * (1 - width_diff_rel)
-                bbox.width = bbox.width * (1 - width_diff_rel)
+                if results.detections:
+                    detection = results.detections[0]
+                    bbox = detection.location_data.relative_bounding_box
 
-                center_x = bbox.xmin + bbox.width / 2
-                center_y = bbox.ymin + bbox.height / 2
-                width = bbox.width
-                height = bbox.height
+                    width_diff_rel = (frame.shape[1] - signer_frame.shape[1]) / frame.shape[1]
+                    bbox.xmin = width_diff_rel + bbox.xmin * (1 - width_diff_rel)
+                    bbox.width = bbox.width * (1 - width_diff_rel)
 
-                bounding_box_buffer.append([center_x, center_y, width, height])
+                    center_x = bbox.xmin + bbox.width / 2
+                    center_y = bbox.ymin + bbox.height / 2
+                    width = bbox.width
+                    height = bbox.height
+
+                    bounding_box_buffer.append([center_x, center_y, width, height])
+            except Exception as e:
+                logging.error(f"Failed to update bounding box at frame {i}: {e}")
+
+            if bounding_box_buffer:
                 smoothed_bbox = moving_average(bounding_box_buffer)
                 max_dimension = max(smoothed_bbox[2], smoothed_bbox[3]) * 3.3
                 crop_size = int(max_dimension * frame_width)
@@ -135,6 +153,7 @@ def process_segment(video_path: str, subtitle_item: SubRipItem, frame_rate: floa
     else:
         return subtitle_item.text
 
+
 def process_video(video_path: str, subtitle_path: str, output_folder: str):
     logging.info(f"Processing video: {video_path}")
 
@@ -159,12 +178,14 @@ def process_video(video_path: str, subtitle_path: str, output_folder: str):
     subs_items = get_subtitle_items(subs)
 
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        futures = []
+        futures: List[Future] = []
         for idx, sub in enumerate(subs_items):
             face_detector = face_detectors[idx % len(face_detectors)]
-            futures.append(executor.submit(process_segment, video_path, sub, frame_rate, frame_width, frame_height, frame_size_multiplier, face_detector, output_folder, idx))
+            futures.append(executor.submit(process_segment, video_path, sub, frame_rate, frame_width, frame_height,
+                                           frame_size_multiplier, face_detector, output_folder, idx))
 
-        for future in as_completed(futures):
+        for future in futures:
+            # Wait for future. We need to keep the order of the subtitles!
             result = future.result()
             if result:
                 subtitle_list.append(result)
@@ -172,6 +193,7 @@ def process_video(video_path: str, subtitle_path: str, output_folder: str):
     with open(os.path.join(output_folder, "subtitles.txt"), 'w') as file:
         for sub in subtitle_list:
             file.write(sub + "\n")
+
 
 def main():
     dataset_path = "."  # Change to the actual dataset path
@@ -206,6 +228,7 @@ def main():
             logging.info(f"Starting processing for video {video_id}")
             process_video(video_file_path, subtitle_file_path, output_folder)
             logging.info(f"Completed processing for video {video_id}")
+
 
 if __name__ == "__main__":
     main()
