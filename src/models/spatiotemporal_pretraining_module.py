5a50262d94206108e76692ff702a67f35452c87e
Lukas Braach <lukas.braach@netlight.com>
2024-05-22 23:43:00 +0200

    I have to fix this serialization error...

--- src/models/spatiotemporal_pretraining_module.py
+++ src/models/spatiotemporal_pretraining_module.py
@@ -1,3 +1,5 @@
+from typing import Dict, Any
+
 import torch
 from lightning import LightningModule
 from torch import nn
@@ -5,25 +7,6 @@ from torchmetrics import MeanMetric
 
 from src.models.components.spatiotemporal_decoder import SpatiotemporalDecoder
 from src.models.components.spatiotemporal_encoder import SpatiotemporalEncoder
-
-
-def mask_latents(latents, mask_ratio=0.3):
-    """
-    Randomly mask latent representations with the specified mask ratio.
-    Args:
-        latents (torch.Tensor): Latent representations of shape (batch_size, num_latents, hidden_size).
-        mask_ratio (float): Proportion of latents to mask.
-
-    Returns:
-        masked_latents (torch.Tensor): Latent representations with some masked out.
-        mask (torch.Tensor): Boolean tensor indicating which latents were masked.
-    """
-    batch_size, num_latents, hidden_size = latents.shape
-    mask = torch.rand(batch_size, num_latents) < mask_ratio
-    masked_latents = latents.clone()
-    masked_latents[mask] = 0  # Mask out the latents
-    
-    return masked_latents, mask
 
 
 class SpatiotemporalPretrainingModule(LightningModule):
@@ -35,6 +18,7 @@ class SpatiotemporalPretrainingModule(LightningModule):
             scheduler: torch.optim.lr_scheduler
     ):
         super().__init__()
+        
         self.encoder = encoder
         self.decoder = decoder
         self.optimizer = optimizer
@@ -68,11 +52,49 @@ class SpatiotemporalPretrainingModule(LightningModule):
         mse_loss = nn.MSELoss()
         return mse_loss(reconstructed_latents[mask], original_latents[mask])
 
-    def configure_optimizers(self):
+    def configure_optimizers(self) -> Dict[str, Any]:
+        """
+        Choose what optimizers and learning-rate schedulers to use in your optimization.
+        Normally you'd need one. But in the case of GANs or similar you might have multiple.
+
+        Examples:
+            https://lightning.ai/docs/pytorch/latest/common/lightning_module.html#configure-optimizers
+
+        :return: A dict containing the configured optimizers and learning-rate schedulers to be used for training.
+        """
         optimizer = self.optimizer(params=self.trainer.model.parameters())
-        scheduler = self.scheduler(optimizer=optimizer)
-        return {"optimizer": optimizer, "lr_scheduler": scheduler}
+        if self.scheduler is not None:
+            scheduler = self.scheduler(optimizer=optimizer)
+            return {
+                "optimizer": optimizer,
+                "lr_scheduler": {
+                    "scheduler": scheduler,
+                    "monitor": "val/loss",
+                    "interval": "epoch",
+                    "frequency": 1,
+                },
+            }
+        return {"optimizer": optimizer}
+
+
+def mask_latents(latents, mask_ratio=0.3):
+    """
+    Randomly mask latent representations with the specified mask ratio.
+    Args:
+        latents (torch.Tensor): Latent representations of shape (batch_size, num_latents, hidden_size).
+        mask_ratio (float): Proportion of latents to mask.
+
+    Returns:
+        masked_latents (torch.Tensor): Latent representations with some masked out.
+        mask (torch.Tensor): Boolean tensor indicating which latents were masked.
+    """
+    batch_size, num_latents, hidden_size = latents.shape
+    mask = torch.rand(batch_size, num_latents) < mask_ratio
+    masked_latents = latents.clone()
+    masked_latents[mask] = 0  # Mask out the latents
+
+    return masked_latents, mask
 
 
 if __name__ == "__main__":
-    _ = SignLanguageLitModule(None, None, None, None)
+    _ = SpatiotemporalPretrainingModule(None, None, None, None)
