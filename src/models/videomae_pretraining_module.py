54c567813ff0800898cf421c051940cdd1b4f31c
Lukas Braach <lukas.braach@netlight.com>
2024-05-31 22:45:41 +0200

    Add VideoMAE preprocessing

--- src/models/videomae_pretraining_module.py
+++ src/models/videomae_pretraining_module.py
@@ -23,6 +23,8 @@ class VideoMAEPretrainingModule(LightningModule):
         self.val_loss = MeanMetric()
 
     def forward(self, pixel_values, bool_masked_pos=None):
+        # expecting pixel_values to be of shape (batch_size, num_frames, 3, 224, 224)
+        pixel_values = self.processor(pixel_values, return_tensors="pt", do_resize=False, do_center_crop=False, do_rescale=False).pixel_values
         return self.model(pixel_values, bool_masked_pos=bool_masked_pos)
 
     def mask_and_forward(self, pixel_values):
