f7e22e9db72a89ab3fbb646301c57596e1e5e4e3
Lukas Braach <lukas.braach@netlight.com>
2024-06-12 10:50:16 +0200

    Add custom VideoMAE

---
+++ src/models/components/custom_videomae.py
@@ -0,0 +1,37 @@
+from typing import Optional, Tuple, Union
+
+import torch
+from transformers import VideoMAEModel
+from transformers.modeling_outputs import BaseModelOutput
+
+
+class CustomVideoMAEModel(VideoMAEModel):
+    def __init__(self, config):
+        super().__init__(config)
+
+    def forward(
+            self,
+            pixel_values: torch.FloatTensor,
+            bool_masked_pos: Optional[torch.BoolTensor] = None,
+            head_mask: Optional[torch.Tensor] = None,
+            output_attentions: Optional[bool] = None,
+            output_hidden_states: Optional[bool] = None,
+            return_dict: Optional[bool] = None,
+            **kwargs,
+    ) -> Union[Tuple, BaseModelOutput]:
+        return super().forward(
+            pixel_values,
+            bool_masked_pos=bool_masked_pos,
+            head_mask=head_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+    def _get_feature_vector_attention_mask(
+            self, feature_vector_length: int, attention_mask: torch.LongTensor, add_adapter=None
+    ):
+        """
+        Just a hack to make SpeechEncoderDecoderModel work with VideoMAEModel
+        """
+        return attention_mask
